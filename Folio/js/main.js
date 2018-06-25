b4af71fe636c29629e0eb636c62c9236b74a1c0b
camckin10 <chelseymckinney2@gmail.com>
2018-06-25 09:12:20 -0500

    pushing intiinitial content to site

--- Folio/js/main.js
+++ Folio/js/main.js
@@ -79,7 +79,7 @@ $(document).ready(function() {
 
   $(function() {
     typed.typed({
-      strings: ["Alex Smith.", "Designer.", "Developer.", "Freelancer.", "Photographer"],
+      strings: ["Chelsey McKinney.", " A Developer", " A Designer", " A Freelancer."],
       typeSpeed: 100,
       loop: true,
     });
